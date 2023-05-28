0ea6428810cc89bf1779467f2b1e3ba5ae19fb63
Владислав Лебедев <vlad-057@mail.ru>
2023-05-29 05:45:26 +0600

    Сделал алг аиснхронным

--- schedule/settings.py
+++ schedule/settings.py
@@ -141,10 +141,10 @@ WSGI_APPLICATION = 'schedule.wsgi.application'
 DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'cu50559_main',
+    'NAME': 'cu50559',
     'USER': 'cu50559',
     'PASSWORD': 'Dagad582#',
-    'HOST': 'localhost',
+    'HOST': '5.23.50.56',
     'PORT': '',
     'OPTIONS': {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",

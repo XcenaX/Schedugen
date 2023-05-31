83649b177b832ce8fa055c2dbf705f078c2ce601
Владислав Лебедев <vlad-057@mail.ru>
2023-05-31 21:20:42 +0600

    pls work

--- schedule/settings.py
+++ schedule/settings.py
@@ -151,12 +151,12 @@ WSGI_APPLICATION = 'schedule.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'main',
         'USER': 'XcenaX',
         'PASSWORD': 'Dagad582#',
         'HOST': '127.0.0.1',      
-        'PORT': '3306'  
+        'PORT': '5432'  
     }
 }
 
